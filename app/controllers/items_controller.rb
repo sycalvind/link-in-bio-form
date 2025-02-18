efcbc54c49208705b0135b1ded276505c8a220a3
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-02-18 20:40:17 +0000

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,18 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def add
+    render({ :template => "item_templates/new" })
+  end
+  def update
+    url = params.fetch("link_url")
+    description = params.fetch("link_description")
+    thumbnail = params.fetch("thumbnail_url")
+    x = Item.new
+    x.link_url = url
+    x.link_description = description
+    x.thumbnail_url = thumbnail
+    x.save
+    redirect_to("/")
+  end
 end
